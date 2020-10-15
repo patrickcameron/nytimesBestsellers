f09340f282f9eda137e51a289059f4d5ad01585e
Patrick Cameron <patcameron@gmail.com>
2020-10-15 10:31:53 -0400

    Improve logic of booktitle pipe

--- src/app/pipes/booktitle.pipe.ts
+++ src/app/pipes/booktitle.pipe.ts
@@ -5,22 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BooktitlePipe implements PipeTransform {
 
-  lowerCaseWords: Array<string> = ['and', 'by', 'from', 'of', 'the'];
+  lowerCaseWords: Array<string> = ['and', 'by', 'from', 'in', 'of', 'the', 'to'];
 
   capitalize(value: string): string {
     return value.charAt(0).toUpperCase() + value.slice(1);
   }
 
   transform(value: string): string {
-    let titleArray: Array<string> = value.split(' ');
-    titleArray.forEach((item, index) => {
-      item = item.toLowerCase();
-      if ( index === 0 || this.lowerCaseWords.indexOf(item) < 0 ) {
-        item = this.capitalize(item);
-      }
-      titleArray[index] = item;
-    })
-    return titleArray.join(' ');
-  }
+    let titleArray: Array<string> = value.toLowerCase().split(/( |-)/);
+    
+    titleArray.forEach((word, index) => {
 
+      if (index === 0) {
+        titleArray[index] = this.capitalize(titleArray[index]);
+      } else if ( (word === ' ' || word === '-') && this.lowerCaseWords.indexOf(titleArray[index + 1]) < 0 ) {
+        titleArray[index + 1] = this.capitalize(titleArray[index + 1]);
+      }
+    });
+    return titleArray.join('');
+  }
 }
