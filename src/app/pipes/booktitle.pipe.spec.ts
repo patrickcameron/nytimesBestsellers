ef28e91c5997fa9edc1d9d1c0beec83264b17f06
Patrick Cameron <patcameron@gmail.com>
2020-10-15 10:33:00 -0400

    Add Jesse's suggested booktitle pipe test functions

--- src/app/pipes/booktitle.pipe.spec.ts
+++ src/app/pipes/booktitle.pipe.spec.ts
@@ -1,32 +1,82 @@
 import { BooktitlePipe } from './booktitle.pipe';
 
 describe('BooktitlePipe', () => {
+  let pipe: BooktitlePipe;
+
+  beforeEach(() => {
+    pipe = new BooktitlePipe();
+  });
+
+  afterEach(() => {
+    pipe = null;
+  });
+
   it('should create', () => {
     const pipe = new BooktitlePipe();
     expect(pipe).toBeTruthy();
   });
-  xit('should convert title from uppercase to proper capitalization', () => {
-    const pipe = new BooktitlePipe();
+
+  it('should convert single word titles to title case', () => {
     const titles = [
+      {
+        title: 'CAPITAL',
+        expected: 'Capital'
+      },
+      {
+        title: 'JACK',
+        expected: 'Jack'
+      },
+      {
+        title: 'THE',
+        expected: 'The'
+      }
+    ]
+    titles.forEach(item => {
+      expect(pipe.transform(item.title)).toEqual(item.expected);
+    })
+  });
+
+  it('should convert multi word tiles to title case', () => {
+    const titles = [
+      {
+        title: 'THE INVISIBLE LIFE OF ADDIE LARUE',
+        expected: 'The Invisible Life of Addie Larue'
+      },
       { 
         title: 'THE EVENING AND THE MORNING',
         expected: 'The Evening and the Morning'
-      },
-      { 
-        title: 'THE COAST-TO-COAST MURDERS',
-        expected: 'The Coast-to-Coast Murders'
       }
-    ];
-    titles.forEach((item) => {
+    ]
+    titles.forEach(item => {
+      expect(pipe.transform(item.title)).toBe(item.expected);
+    })
+  });
+
+  it('should not convert select words to title case unless they are first word in title', () => {
+    const titles = [
+      {
+        title: 'THE EVENING AND THE MORNING',
+        expected: 'The Evening and the Morning'
+      },
+      {
+        title: 'THE BOOK OF TWO WAYS',
+        expected: 'The Book of Two Ways'
+      }
+    ]
+    titles.forEach(item => {
       expect(pipe.transform(item.title)).toEqual(item.expected);
     })
   })
 
-  it('should convert single word titles to title case');
-
-  it('should convert multi word tiles to title case');
-
-  it('should not convert select words to title case')
-
-  it('should treat hyphens as word delimiters')
+  it('should treat hyphens as word delimiters', () => {
+    const titles = [
+      { 
+        title: 'THE COAST-TO-COAST MURDERS',
+        expected: 'The Coast-to-Coast Murders'
+      }
+    ]
+    titles.forEach(item => {
+      expect(pipe.transform(item.title)).toEqual(item.expected);
+    })
+  })
 });
