e57cad7213166165435c68c7a82941e9ce19b59c
Patrick Cameron <patcameron@gmail.com>
2020-10-19 15:12:55 -0400

    Add more tests for Navbar component

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -2,11 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('Component: Navbar', () => {
     let component: NavbarComponent;
     let fixture: ComponentFixture<NavbarComponent>;
     let mockFirebaseService: jasmine.SpyObj<FirebaseService>;
+    let loggedInButtonsEl: DebugElement;
+    let loggedOutButtonsEl: DebugElement;
 
     beforeEach(async(() => {
         mockFirebaseService = jasmine.createSpyObj(
@@ -46,4 +50,26 @@ describe('Component: Navbar', () => {
         component.logout();
         expect(mockFirebaseService.logout).toHaveBeenCalled();
     });
+
+    describe('when user is logged in', () => {
+        it('should display My Books and Logout buttons', () => {
+            component.isLoggedIn = true;
+            fixture.detectChanges();
+            loggedInButtonsEl = fixture.debugElement.query(By.css('.buttons.is-logged-in'));
+            loggedOutButtonsEl = fixture.debugElement.query(By.css('.buttons.is-logged-out'));
+            expect(loggedInButtonsEl).toBeDefined();
+            expect(loggedOutButtonsEl).toBeNull();
+        })
+    });
+
+    describe('when user is logged out', () => {
+        it('should display Sign Up and Log In buttons', () => {
+            component.isLoggedIn = false;
+            fixture.detectChanges();
+            loggedOutButtonsEl = fixture.debugElement.query(By.css('.buttons.is-logged-out'));
+            loggedInButtonsEl = fixture.debugElement.query(By.css('.buttons.is-logged-in'));
+            expect(loggedOutButtonsEl).toBeDefined();
+            expect(loggedInButtonsEl).toBeNull();
+        })
+    })
 });
